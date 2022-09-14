1902b744bf95393d36ccb3e831cdbfd39756ea09
Simon-Boyer <Simon-Boyer@users.noreply.github.com>
2022-09-14 04:00:49 +0000

    deploy: 12e4c51ef373292597d9037e34084ca2208a89e8

---
+++ sendgrid.js
@@ -0,0 +1,29 @@
+#! /usr/bin/env node
+
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+const data = {
+    name: "Blog Simon",
+    send_to: {
+        list_ids: ['de8ca425-5ab0-4d10-a0cd-27a8cf77bfdc']
+    },
+    email_config: {
+      design_id: 'b983dfb7-42fe-4172-a5f5-e2d07b92dfb2'
+    }
+  };
+  
+const request = {
+    url: `/v3/marketing/singlesends`,
+    method: 'POST',
+    body: data
+}
+  
+sgMail.request(request)
+  .then(([response, body]) => {
+    console.log(response.statusCode);
+    console.log(response.body);
+  })
+  .catch(error => {
+    console.error(error);
+  });
